98479f1d9df6bdeaa9010aeafa4633a7e952c22b
Ian <ihafkenschiel@gmail.com>
2020-01-04 18:48:00 -0800

    Contest - WiP

---
+++ contest/freqAlphabets.js
@@ -0,0 +1,82 @@
+/**
+5303. Decrypt String from Alphabet to Integer Mapping
+
+    User Accepted: 1
+    User Tried: 5
+    Total Accepted: 1
+    Total Submissions: 5
+    Difficulty: Easy
+
+Given a string s formed by digits ('0' - '9') and '#' . We want to map s to English lowercase characters as follows:
+
+    Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+    Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+
+Return the string formed after mapping.
+
+It's guaranteed that a unique mapping will always exist.
+
+
+
+Example 1:
+
+Input: s = "10#11#12"
+Output: "jkab"
+Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+
+Example 2:
+
+Input: s = "1326#"
+Output: "acz"
+
+Example 3:
+
+Input: s = "25#"
+Output: "y"
+
+Example 4:
+
+Input: s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"
+Output: "abcdefghijklmnopqrstuvwxyz"
+
+
+
+Constraints:
+
+    1 <= s.length <= 1000
+    s[i] only contains digits letters ('0'-'9') and '#' letter.
+    s will be valid string such that mapping is always possible.
+
+
+*/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var freqAlphabets = function(s) {
+  var str = "";
+
+  var arr = s.split("#");
+  console.log(arr.toString());
+
+  for (j=0; j<arr.length; j++) {
+    var cur = Number(arr[j]);
+    console.log(cur);
+    if ( j != arr.length-1 && cur >= 10 && cur <= 26) {
+      str += String.fromCharCode(96 + cur);
+    } else {
+      var grop = arr[j];
+      for (i=0; i<grop.length; i++) {
+         console.log(Number(grop.charAt(i)));
+         str += String.fromCharCode(96 + Number(grop.charAt(i)));
+       }
+    }
+  }
+
+
+
+    return str;
+};
+
+document.write(freqAlphabets("10#11#12"));
